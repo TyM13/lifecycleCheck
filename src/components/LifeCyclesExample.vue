8172851527a99833d3bf143fa5c65ba9eea5c237
Your Name <you@example.com>
2022-08-09 18:39:42 -0600

    knowledge check lifecycles example

---
+++ src/components/LifeCyclesExample.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  beforeCreate: function () {
+    console.log(`Currently in beforeCreate`);
+  },
+  created: function () {
+    console.log(`Currently in Create`);
+  },
+beforeMount: function () {
+console.log(`Currently in beforeMount`);
+},
+mounted: function () {
+console.log(`Currently in mounted`);
+},
+
+
+  name: "life-cycles-example",
+};
+</script>
+
+<style scoped>
+</style>
